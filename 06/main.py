87a4ffffbf045340365f0af2b07ea41e5e201697
= <erik.aho24@gmail.com>
2024-12-06 08:52:08 -0800

    Clean up Day 6

--- 06/main.py
+++ 06/main.py
@@ -4,15 +4,13 @@ import numpy as np
 
 
 def main(input: str):
-    map_arr = np.array([
+    map_arr = np.rot90(np.array([
         list(x) for x in input.strip().split("\n")
-    ])
+    ]), k=-1) # rotate matrix, as it comes in rotated by default for some reason
 
-    up = (-1, 0) # ive somehow rotated this in the above
-
-    start_pos = np.where(map_arr == "^")
-    start_pos = start_pos[0][0], start_pos[1][0]
-    start_dir = up
+    guard_start = np.where(map_arr == "^")
+    start_pos = guard_start[0][0], guard_start[1][0]
+    start_dir = (0, 1)
 
     def take_guard_step(pos, dir, map):
         possible_next_step = pos[0] + dir[0], pos[1] + dir[1]
@@ -27,7 +25,6 @@ def main(input: str):
 
     seen = set()
     seen.add(start_pos)
-
     pos = start_pos
     dir = start_dir
 
@@ -36,11 +33,11 @@ def main(input: str):
         if pos != "OOB":
             seen.add(pos)
 
+    print(f"Part 1: {len(seen)}")
+
+    # p2
     def check_if_loops(block, map):
-        try:
-            map[block] = "#"
-        except Exception:
-            return False
+        map[block] = "#"
         seen_with_dir = set()
         pos, dir = start_pos, start_dir
         seen_with_dir.add((pos, dir))
@@ -57,7 +54,6 @@ def main(input: str):
         if check_if_loops(potential_block, map_arr.copy()):
             count_loops += 1
 
-    print(f"Part 1: {len(seen)}")
     print(f"Part 2: {count_loops}")
 
 
