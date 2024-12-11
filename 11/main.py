e5c626959daa70d9d36a39ea189e2d16335223f0
= <erik.aho24@gmail.com>
2024-12-10 21:33:50 -0800

    Renaming vars

--- 11/main.py
+++ 11/main.py
@@ -7,21 +7,21 @@ def main(input: str):
     input = [int(x) for x in input.split(" ")]
 
     @functools.cache
-    def recursive_final_num(stone, i):
-        if i == 0:
+    def recursive_count(stone, t):
+        if t == 0:
             return 1
         if stone == 0:
-            return recursive_final_num(1, i - 1)
+            return recursive_count(1, t - 1)
 
         s, l = str(stone), len(str(stone))
         if l % 2 == 0:
-            return (recursive_final_num(int(s[:l // 2]), i - 1) +
-                    recursive_final_num(int(s[l // 2:]), i - 1))
+            return (recursive_count(int(s[:l // 2]), t - 1) +
+                    recursive_count(int(s[l // 2:]), t - 1))
 
-        return recursive_final_num(stone * 2024, i - 1)
+        return recursive_count(stone * 2024, t - 1)
 
-    print(f"P1: {sum([recursive_final_num(stone, 25) for stone in input])}")
-    print(f"P2: {sum([recursive_final_num(stone, 75) for stone in input])}")
+    print(f"P1: {sum([recursive_count(stone, 25) for stone in input])}")
+    print(f"P2: {sum([recursive_count(stone, 75) for stone in input])}")
 
 
 if __name__ == "__main__":
