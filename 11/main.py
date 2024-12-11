51c94ad17cdcb13a4297bf81628f735ca79afe41
= <erik.aho24@gmail.com>
2024-12-10 21:31:08 -0800

    Day 11

--- 11/main.py
+++ 11/main.py
@@ -1,9 +1,28 @@
+import functools
+
 from utils import run_day
 
 
 def main(input: str):
-    pass
+    input = [int(x) for x in input.split(" ")]
+
+    @functools.cache
+    def recursive_final_num(stone, i):
+        if i == 0:
+            return 1
+        if stone == 0:
+            return recursive_final_num(1, i - 1)
+
+        s, l = str(stone), len(str(stone))
+        if l % 2 == 0:
+            return (recursive_final_num(int(s[:l // 2]), i - 1) +
+                    recursive_final_num(int(s[l // 2:]), i - 1))
+
+        return recursive_final_num(stone * 2024, i - 1)
+
+    print(f"P1: {sum([recursive_final_num(stone, 25) for stone in input])}")
+    print(f"P2: {sum([recursive_final_num(stone, 75) for stone in input])}")
 
 
 if __name__ == "__main__":
-    run_day(main, run_dev=True)
+    run_day(main, run_dev=False, parse_input=False)
